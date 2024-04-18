c60d5d9f250667b0e52af20c0d08b744de837255
Ksenia <skichkoksenia@yandex.ry>
2024-04-18 13:28:56 +0300

    One

---
+++ sitepet/users/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    photo = models.ImageField(upload_to='users/%d/%m/%Y', blank=True, null=True, verbose_name='Фотография')
+    happy_birth = models.DateTimeField(null=True, blank=True, verbose_name='Дата рождения')
