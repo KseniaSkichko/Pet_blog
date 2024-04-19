6c3b8e50ddd1e13f4b8bcbe80d594fca94dee346
Ksenia <skichkoksenia@yandex.ry>
2024-04-19 08:42:56 +0300

    4

--- sitepet/pet/models.py
+++ sitepet/pet/models.py
@@ -1,26 +1,81 @@
 from django.db import models
 
 
+#Проверяем опубликовано или приватно
+class PublicationManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(publication=Material.Publiks.PRIVAT)
 
+#Модель описывает публикации(контент
 class Material(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    publication = models.BooleanField(default=False)
+    class Publiks(models.IntegerChoices):
+        PRIVAT = 0, 'Приватно'
+        PUBLISHED = 1, 'Опубликовано'
+    title = models.CharField(max_length=255)    #название
+    slug = models.SlugField(max_length=70, unique=True, db_index=True)   #слаг (запись сверху, в поле поиска
+    content = models.TextField(blank=True)     #текс записи (описания)
+    time_create = models.DateTimeField(auto_now_add=True)     #время создания
+    time_update = models.DateTimeField(auto_now=True)       #время обновления
+    publication = models.BooleanField(choices=Publiks.choices,
+                                      default=Publiks.PUBLISHED)   #приватно или нет
+    typepost = models.ForeignKey('TypePost', on_delete=models.PROTECT,
+                                 related_name='posts')   #тип поста
+    tags = models.ManyToManyField('TagPosts', blank=False, null=False,
+                                  related_name='tags')
+
+    objects = models.Manager()
+    publik = PublicationManager()
 
     def __str__(self):
         return self.title
 
 
+#Сортируем по времени публикации (свежие записи-время больше находятся выше)
+    def Meta():
+        ordering = ['-time_create']
+        indexes = [
+            models.Index(fields=['-time_create'])
+        ]
+
+
+#Модель описывает питомца
 class MyPet(models.Model):
-    name = models.CharField(null=False, blank=False, max_length=50)
-    bread = models.CharField(null=True, blank=True, max_length=50)
-    photo = models.ImageField(upload_to='users/%d/%m/%Y', blank=True, null=True, verbose_name='Фотография')
-    happy_birth = models.DateTimeField(null=True, blank=True, verbose_name='Дата рождения')
+    name = models.CharField(null=False, blank=False, max_length=50) #Имя
+    animal = models.OneToOneField('Animal', on_delete=models.PROTECT,
+                                  null=False,
+                                  blank=False, related_name='animal',
+                                  max_length=50)
+    bread = models.CharField(null=True, blank=True, max_length=50)     #Порода
+    photo = models.ImageField(upload_to='users/%d/%m/%Y', blank=True,
+                              null=True, verbose_name='Фотография')
+    happy_birth = models.DateTimeField(null=True, blank=True,
+                                       verbose_name='Дата рождения')
     character = models.CharField(blank=False, null=False, max_length=500)
     can = models.CharField(blank=False, null=False, max_length=500)
     delicacy = models.CharField(blank=False, null=False, max_length=500)
     byaka = models.CharField(blank=True, max_length=500)
     favorite = models.CharField(blank=True, max_length=500)
+
+
+class TypePost(models.Model):
+    name = models.CharField(max_length=70, db_index=True)
+    slug = models.SlugField(max_length=70, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+
+class TagPosts(models.Model):
+    tag = models.CharField(max_length=30, db_index=True)
+    slug = models.SlugField(max_length=70, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.tag
+
+
+class Animal(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
--- sitepet/sitepet/urls.py
+++ sitepet/sitepet/urls.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from pet.views import page_does_not_exist
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pet.urls')),
 ]
 
-
-handler404 = page_does_not_exist()
+handler404 = page_does_not_exist
